9b523bf0c8ac915eb60f906d9c619b92283b539a
Gamal <gamalalaa2005@gmail.com>
2025-12-05 02:16:24 +0200

    Fixed Customer

--- untitled/src/Customer.java
+++ untitled/src/Customer.java
@@ -1,24 +1,28 @@
 import java.util.List;
+import java.util.ArrayList;
 
 public class Customer {
-    // attibutes declaration
-    int CustomerID;
-    String Name;
-    String Address;
-    List<Order> orders;
-    public Customer(int CustomerID,String Name,String Address){
-        this.CustomerID=CustomerID;
-        this.Name=Name;
-        this.Address=Address;
-        orders=new List<Order>();
+    // attributes declaration
+    private int customerID;
+    private String name;
+    private String address;
+    private List<Order> orders;
+
+    public Customer(int customerID, String name, String address) {
+        this.customerID = customerID;
+        this.name = name;
+        this.address = address;
+        this.orders = new ArrayList<>();
     }
+
     public void placeOrder(Order order) {
         orders.add(order);
         System.out.println("New Order Placed");
     }
-    public void displayOrders(){
-        for(i in orders)
+
+    public void displayOrders() {
+        for (Order order : orders) {
+            System.out.println(order);
+        }
     }
-
-
 }
